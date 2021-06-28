5d776251efe473773889a2931235d6e4b0c79762
Chelsea L <chelsealumsden@gmail.com>
2021-06-28 12:03:59 -0400

    Backend is done

---
+++ server.js
@@ -0,0 +1,34 @@
+// Dependencies
+const express = require ("express");
+const cors = require("cors");
+
+// Json
+const projects = require("./projects.json");
+const about = require("./about.json");
+
+// Create Express App
+const app = express();
+
+// Middleware
+app.use(cors());
+
+// Routes
+
+// --- Home ---
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+// --- Project ---
+app.get('/projects', (req, res) => {
+    res.json(projects);
+});
+
+// --- About ---
+app.get('/about', (req, res) => {
+    res.json(about);
+});
+
+// Listen
+const PORT = process.env.PORT || 4000;
+app.listen(PORT), () => console.log(`Listen to port on:${PORT}`)
